72e0c6d809b8ab4ffac3b693e4e1bafb3f87cfd5
alhassanyoussef <hyoussef650@.com>
2023-07-18 16:51:47 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,10 +23,10 @@ void times_table(void)
 			if (z <= 9)
 				_putchar(' ');
 			else
-				_putchar((y / 10) + 48);
+				_putchar(y / 10 + '0');
 
 
-				_putchar((y % 10) + 48);
+				_putchar(y % 10 + '0');
 
 		}
 		_putchar('\n');
