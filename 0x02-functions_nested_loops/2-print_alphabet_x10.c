3c9b8ef2f4c317ae6f24041deea46f293cf0cce0
alhassanyoussef <hyoussef650@.com>
2023-07-18 01:49:04 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * print_alphabet_x10 - utilizes _putchar to prints 10 times the alphabet,
+ *                                              in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int x, ch;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+		_putchar('\n');
+	}
+
+}
