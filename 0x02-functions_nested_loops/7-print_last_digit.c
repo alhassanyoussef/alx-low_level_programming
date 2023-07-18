8235500f792c308a14c9bc7818454a37a9ad3478
alhassanyoussef <hyoussef650@.com>
2023-07-18 14:31:29 -0700

    a

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,11 @@
 int print_last_digit(int n)
 {
 	int lastdigit;
+
 	if (n < 0)
 		lastdigit = -1 * (n % 10);
 	else
-		n = n % 10;
+		lastdigit = n % 10;
 
 	_putchar(lastdigit + '0');
 	return (lastdight);
