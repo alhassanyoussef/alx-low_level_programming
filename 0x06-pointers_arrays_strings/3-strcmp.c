d4fa3c99995556919426a52aa16c06d7b48367c2
alhassanyoussef <hyoussef650@.com>
2023-07-26 23:56:01 +0300

    a

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcmp - makes comparison between two strings
+ *
+ * @s1: string
+ * @s2: string
+ *
+ * Return: (0)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+
+	return (strcmp(s1, s2));
+}
