130d17a58dc99d24d295e1f2d3de99315ce31505
alhassanyoussef <hyoussef650@.com>
2023-07-26 21:55:18 +0300

    a

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - makes concatenate for two strings or variable
+ *
+ * @dest: string
+ *
+ * @src: string
+ *
+ * Return: (dest)
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (dest);
+
+}
