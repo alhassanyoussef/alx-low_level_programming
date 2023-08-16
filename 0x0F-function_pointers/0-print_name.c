cb8a1fdfc5deae4510c85440e8eec4fd0ddd4820
alhassanyoussef <hyoussef650@.com>
2023-08-16 22:33:41 +0300

    a

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -12,12 +12,3 @@ void print_name(char *name, void (*f)(char *))
 {
 	f(name);
 }
-/**
- * print - funcion that print
- * @name: name that printed
- */
-
-void print(char *name)
-{
-	printf("%s\n", name);
-}
