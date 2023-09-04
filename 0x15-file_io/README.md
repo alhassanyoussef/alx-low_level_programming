a63164842dd0e2380d34e704fcd7649199850698
alhassanyoussef <hyoussef650@.com>
2023-09-05 00:11:24 +0300

    hellow, world

---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+this for file i/o
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define READ_BUF_SIZE 1024
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+
+
+#endif
